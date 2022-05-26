e07b6ff7b43288a3a7c3f0d6397eab5cfecd8800
Vikash <vikashpathak041@gmail.com>
2022-05-26 14:30:16 +0530

    check option

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,17 +3,41 @@ package com.bridgelabz.SnakeAndLadder;
 //Day 4 Snake And Ladder Simulator
 public class SnakeAndLadder
 {
-  public static int getRandomInteger(int maximum, int minimum){
-      return ((int) (Math.random()*(maximum - minimum))) + minimum;
-  }
+    private static final int NO_PLAY = 0;
+    private static final int LADDER = 1;
+    private static final int SNAKE = 2;
 
-  public static void main(String[] args) {
+    public static int getRandomInteger(int maximum, int minimum)
+    {
+        return ((int) (Math.random()*(maximum - minimum))) + minimum;
+    }
 
-      System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+    public static void main(String[] args)
+    {
 
-      int startPositionA = 0;
-      int dice = getRandomInteger(7,1);
-      System.out.println(dice);
+        System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
-  }
+        int startPositionA = 0;
+        int dice = getRandomInteger(7,1);
+        System.out.println(dice);
+        int checkPlay = (int) Math.floor(Math.random()*10)%3;
+        System.out.println(checkPlay);
+
+        switch(checkPlay)
+        {
+            case NO_PLAY:
+                break;
+
+            case LADDER:
+                startPositionA += dice;
+                break;
+
+            case SNAKE:
+                startPositionA -= dice;
+                break;
+        }
+
+    }
+    //System.out.println(startPositionA);
+
 }
