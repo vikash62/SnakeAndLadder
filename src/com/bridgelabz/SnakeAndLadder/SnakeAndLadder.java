ac1d8d4217815f0cf3a12dd4734ea7c89085d0c6
Vikash <vikashpathak041@gmail.com>
2022-05-26 14:34:21 +0530

    repeat untill any player win

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,41 +3,45 @@ package com.bridgelabz.SnakeAndLadder;
 //Day 4 Snake And Ladder Simulator
 public class SnakeAndLadder
 {
-    private static final int NO_PLAY = 0;
-    private static final int LADDER = 1;
-    private static final int SNAKE = 2;
+  private static final int NO_PLAY = 0;
+  private static final int LADDER = 1;
+  private static final int SNAKE = 2;
+  private static final int MAX_POSITION = 100;
+  private static final int MIN_POSITION = 0;
 
-    public static int getRandomInteger(int maximum, int minimum)
-    {
-        return ((int) (Math.random()*(maximum - minimum))) + minimum;
-    }
+  public static int getRandomInteger(int maximum, int minimum){
+      return ((int) (Math.random()*(maximum - minimum))) + minimum;
+  }
 
-    public static void main(String[] args)
-    {
+  public static void main(String[] args) {
 
-        System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+      System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
-        int startPositionA = 0;
-        int dice = getRandomInteger(7,1);
-        System.out.println(dice);
-        int checkPlay = (int) Math.floor(Math.random()*10)%3;
-        System.out.println(checkPlay);
+      int positionA = 0;
 
-        switch(checkPlay)
-        {
-            case NO_PLAY:
-                break;
+      while(positionA < MAX_POSITION) {
 
-            case LADDER:
-                startPositionA += dice;
-                break;
+          int dice = getRandomInteger(7,1);
+          System.out.println("dice : "+dice);
+          int checkPlay = (int) Math.floor(Math.random()*10)%3;
+          System.out.println("checkplay : "+checkPlay);
 
-            case SNAKE:
-                startPositionA -= dice;
-                break;
-        }
+          switch(checkPlay) {
+              case NO_PLAY:
+                  break;
 
-    }
-    //System.out.println(startPositionA);
+              case LADDER:
+                  positionA += dice;
+                  break;
 
+              case SNAKE:
+                  positionA -= dice;
+                  if(positionA < MIN_POSITION )
+                      positionA = 0;
+                  break;
+          }
+          System.out.println("end : "+positionA);
+      }
+
+  }//System.out.println(startPositionA);
 }
