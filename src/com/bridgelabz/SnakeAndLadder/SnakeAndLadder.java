0d9417637d8771353ebff9acfd17e2dad2573d02
Vikash <vikashpathak041@gmail.com>
2022-05-26 14:27:04 +0530

    generate random no

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,12 +3,17 @@ package com.bridgelabz.SnakeAndLadder;
 //Day 4 Snake And Ladder Simulator
 public class SnakeAndLadder
 {
-  public static void main(String[] args)
-  {
+  public static int getRandomInteger(int maximum, int minimum){
+      return ((int) (Math.random()*(maximum - minimum))) + minimum;
+  }
+
+  public static void main(String[] args) {
 
       System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
       int startPositionA = 0;
+      int dice = getRandomInteger(7,1);
+      System.out.println(dice);
 
   }
 }
