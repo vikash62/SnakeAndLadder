8a2efa74ec0c7996e602038a8d94ccb7a14e20f2
Vikash <vikashpathak041@gmail.com>
2022-05-26 14:23:49 +0530

    start game

--- src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.SnakeAndLadder;
 
-public class SnakeAndLadder {
+//Day 4 Snake And Ladder Simulator
+public class SnakeAndLadder
+{
+  public static void main(String[] args)
+  {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("SnakeAndLadder");
+      System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
-	}
+      int startPositionA = 0;
 
+  }
 }
